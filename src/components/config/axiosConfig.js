4a21af93ce80e87da76f8d401a7fa6b5e5152599
fixmylifedesigns <ijd.irving@gmail.com>
2020-05-07 01:31:26 -0400

    headers edit

--- src/components/config/axiosConfig.js
+++ src/components/config/axiosConfig.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
 function axiosWithAuth() {
-//   const token = localStorage.getItem("token");
+  //   const token = localStorage.getItem("token");
 
   return axios.create({
     baseURL: process.env.REACT_APP_DOMAIN_NAME,
-    // headers: {
-    //   "Content-Type": "application/json",
-    //   Authorization: `${token}`,
-    // },
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Credentials": true,
+    },
     withCredentials: true,
   });
 }
@@ -21,14 +21,13 @@ function axiosWithoutAuth() {
 
 function axiosViewsSession() {
   axiosWithAuth()
-  .post("/views/", {
-    shop_id: parseInt(process.env.REACT_APP_USER_ID),
-  })
-  // .then((res) => {
-  //   console.log(res);
-  // })
-  .catch((err) => console.log(err));
-  
+    .post("/views/", {
+      shop_id: parseInt(process.env.REACT_APP_USER_ID),
+    })
+    // .then((res) => {
+    //   console.log(res);
+    // })
+    .catch((err) => console.log(err));
 }
 
 export { axiosWithAuth, axiosWithoutAuth, axiosViewsSession };
